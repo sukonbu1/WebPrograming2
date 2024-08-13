e6930432365a607c13e958e3d18d74fb8b9c9767
HuyNguyen <duchuy1912.hn@gmail.com>
2024-08-13 19:12:40 +0700

    Initial commit

---
+++ client/src/components/OtherProducts/OtherProducts.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container mt-4">
+      <h2>Other products</h2>
+      <div v-if="others && others.length">
+        <div class="row">
+          <div class="col-lg-3 mb-4" v-for="other in others" :key="other._id">
+            <router-link :to="'/product-detail/' + other._id" class="card-link">
+                <div class="card">
+                    <img :src="other.images[0]" class="card-img-top" alt="Image">
+                    <div class="card-body">
+                        <h5 class="item-title">{{ other.name }}</h5>
+                        <p class="card-text-price">{{ other.price }} USD</p>
+                    </div>
+                </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <p>No Playstation accessories available at the moment.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { fetchProductsByCategory } from '../../helpers/api'; 
+  
+  export default {
+    name: 'OtherProducts',
+    data() {
+      return {
+        others: []
+      };
+    },
+    async mounted() {
+      try {
+        this.others = await fetchProductsByCategory('Others', ' ');
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    }
+  };
+  </script>
+  
+
+  
---
+++ client/src/components/Xbox/XboxAccessories.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container mt-4">
+      <h2>Xbox Accessories</h2>
+      <div v-if="consoles && consoles.length">
+        <div class="row">
+          <div class="col-lg-3 mb-4" v-for="console in consoles" :key="console._id">
+            <router-link :to="'/product-detail/' + console._id" class="card-link">  
+              <div class="card">
+                <img :src="console.images[0]" class="card-img-top" alt="Console Image">
+                <div class="card-body">
+                  <h5 class="item-title">{{ console.name }}</h5>
+                  <p class="item-category">{{ console.category }}</p>
+                  <p class="card-text-price">{{ console.price }} USD</p>
+                </div>
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <p>No Xbox accessories available at the moment.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { fetchProductsByCategory } from '../../helpers/api'; 
+  
+  export default {
+    name: 'XboxAccessories',
+    data() {
+      return {
+        consoles: []
+      };
+    },
+    async mounted() {
+      try {
+        this.consoles = await fetchProductsByCategory('Xbox', 'Accessories');
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    }
+  };
+  </script>
+  
